7829ae2f16be60afe2f8b2801a2681ceb788c620
daseulll <nldaseul@gmail.com>
2021-10-25 07:37:30 +0900

    Initialize Spring Boot Project

---
+++ my-notice-board/src/main/java/net/board/MyNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package net.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNoticeBoardApplication.class, args);
+	}
+
+}
---
+++ my-notice-board/src/test/java/net/board/MyNoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package net.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
